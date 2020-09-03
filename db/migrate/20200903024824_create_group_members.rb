5329d8d70c17eb992c49db75ab30873ca8c02083
Awtanabe <panser098@gmail.com>
2020-09-03 11:52:26 +0900

    中間テーブル作成

---
+++ db/migrate/20200903024824_create_group_members.rb
@@ -0,0 +1,11 @@
+class CreateGroupMembers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :group_members do |t|
+      t.references :group, null: false, foreign_key: true
+      t.references :member, null: false, foreign_key: true
+      t.boolean :is_head
+      t.datetime :is_head_at
+      t.timestamps
+    end
+  end
+end
