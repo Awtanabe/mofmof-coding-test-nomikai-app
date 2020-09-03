f570ae3fbe1a321f17de4e18db6306e3678d0da6
Awtanabe <panser098@gmail.com>
2020-09-03 21:23:28 +0900

    グループに追加できるようにしました

--- app/controllers/group_members_controller.rb
+++ app/controllers/group_members_controller.rb
@@ -5,6 +5,22 @@ class GroupMembersController < ApplicationController
     @unmembers = GroupMember.unmembers(params[:group_id])
   end
 
+  def create
+    @group_member = GroupMember.new(group_id: params[:group_id], member_id: params[:member_id])
+
+    if @group_member.save
+      flash[:success] = 'グループ参加に成功しました' 
+    else
+      flash[:failed] = 'グループ参加に失敗しました' 
+    end
+    redirect_to group_members_path(group_id: params[:group_id])
+  end
+  def destroy
+    group_member = GroupMember.where(group_id: params[:group_id], member_id: params[:id]).first
+    group_member.destroy
+    redirect_to group_members_path(group_id: params[:group_id])
+  end
+
   def add_head
     @organizer = GroupMember.chose_organizer(params[:group_id])
     if @organizer.blank?
@@ -13,11 +29,5 @@ class GroupMembersController < ApplicationController
       @organizer.set_organizer
     end
       redirect_to group_members_path(group_id: params[:id])
-  end
-
-  def destroy
-    group_member = GroupMember.where(group_id: params[:group_id], member_id: params[:id]).first
-    group_member.destroy
-    redirect_to group_members_path(group_id: params[:group_id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'members#index'
   resources :members
   resources :groups
-  resources :group_members, only: [:index, :destroy] do
+  resources :group_members, only: [:index, :create,:destroy] do
     member do
       get :add_head
     end
